0cded85fafa9bf28341afef384c8669ee7ad7e94
divyanshi724 <divyanshiisaini1815@gmail.com>
2025-01-27 21:45:25 +0530

    Add files via upload

---
+++ methodpractice.java
@@ -0,0 +1,16 @@
+
+public class methodpractice {
+    static int max(int x,int y){
+        return x>y?x:y;
+    }
+    static float max(float x,float y){
+        if(x>y)
+        return x;
+        else
+        return y;
+    }
+    public static void main(String[] args) {
+        System.out.println(max(10.5f,5.4f));
+    }
+    
+}
