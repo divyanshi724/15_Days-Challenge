66af2557900ad83b7470b95c3977e7b0e7e7ab7d
divyanshi724 <divyanshiisaini1815@gmail.com>
2025-02-02 22:12:37 +0530

    Add files via upload

---
+++ RecursionExample.java
@@ -0,0 +1,16 @@
+public class RecursionExample {
+
+    // Recursive function to calculate factorial
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1; // Base case: factorial of 0 is 1
+        } else {
+            return n * factorial(n - 1); // Recursive case
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is: " + factorial(number));
+    }
+}
